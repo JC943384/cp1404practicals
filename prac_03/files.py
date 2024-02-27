c3209e559bb2acda7375ea8abfd247ca2f7068a8
sjh <jiahao.song@my.jcu.edu.au>
2024-02-27 15:37:10 +0800

    finished file.py

---
+++ prac_03/files.py
@@ -0,0 +1,28 @@
+NAME_FILE = "name.txt"
+out_file = open(NAME_FILE, "w")
+name = input("Please enter your name: ")
+print(name, file=out_file)
+out_file.close()
+
+with open('name.txt', 'r') as out_file:
+    name = out_file.read()
+    # Print the name
+    print("Your name is", name)
+
+with open("numbers.txt", 'r') as file:
+    total = 0
+    for i in range(2):
+        num = int(file.readline())
+        total += num
+    print("first two numbers adds:", total)
+
+with open("numbers.txt", 'r') as out_file:
+    total = 0
+    for line in out_file:
+        num = int(line)
+        total += num
+    print("The total sum of all numbers in numbers.txt is:", total)
+
+
+
+
